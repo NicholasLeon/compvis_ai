e1d0ccfd0d320f27a47bb8a9a2964552eecada56
NicholasLeon <nicholasleonard04@gmail.com>
2025-11-15 16:01:16 +0700

    add gitignore and new testing image

--- backend/yolo.py
+++ backend/yolo.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import os
 
 
-image_path = os.path.abspath("test.jpg") #dummy image
+image_path = os.path.abspath("testing.jpg") #dummy image
 model = YOLO("yolo11n.pt")
 
 results = model(image_path)
